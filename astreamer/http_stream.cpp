d958b867a0da1f9dde66e192c2bb3d72d1494893
Matias Muhonen <mmu@iki.fi>
2013-11-08 14:45:29 +0200

    Comment.

--- astreamer/http_stream.cpp
+++ astreamer/http_stream.cpp
@@ -9,7 +9,7 @@
 #include "id3_parser.h"
 
 /*
- * Uncomment the following line to enable the experimental ID3 tag support
+ * Comment the following line to disable ID3 tag support:
  */
 #define INCLUDE_ID3TAG_SUPPORT 1
 
