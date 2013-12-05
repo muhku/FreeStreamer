f43b2239f93e53b8f59828bfa3e05dd6f7ad34fc
Matias Muhonen <mmu@iki.fi>
2013-12-05 11:55:09 +0200

    Document FSParseRssPodcastFeedRequest.

--- Common/FSParseRssPodcastFeedRequest.h
+++ Common/FSParseRssPodcastFeedRequest.h
@@ -6,10 +6,21 @@
 
 #import "FSXMLHttpRequest.h"
 
+/**
+ * Use this request for retrieving the contents for a podcast RSS feed.
+ * Upon request completion, the resulting playlist items are
+ * in the playlistItems property.
+ *
+ * See the FSXMLHttpRequest class how to form a request to retrieve
+ * the RSS feed.
+ */
 @interface FSParseRssPodcastFeedRequest : FSXMLHttpRequest {
     NSMutableArray *_playlistItems;
 }
 
+/**
+ * The playlist items stored in the FSPlaylistItem class.
+ */
 @property (readonly) NSMutableArray *playlistItems;
 
 @end
