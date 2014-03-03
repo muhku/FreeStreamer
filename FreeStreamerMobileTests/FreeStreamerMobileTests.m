7cbb3a75edc77d83a24f5f55da239bf1c9d235d7
Matias Muhonen <mmu@iki.fi>
2014-03-04 00:08:44 +0200

    Recreate the mobile project due to Xcode still failing to execute the tests.

---
+++ FreeStreamerMobileTests/FreeStreamerMobileTests.m
@@ -0,0 +1,31 @@
+/*
+ * This file is part of the FreeStreamer project,
+ * (C)Copyright 2011-2014 Matias Muhonen.
+ * See the file ''LICENSE'' for using the code.
+ */
+
+#import <XCTest/XCTest.h>
+
+@interface FreeStreamerMobileTests : XCTestCase
+
+@end
+
+@implementation FreeStreamerMobileTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+}
+
+@end
