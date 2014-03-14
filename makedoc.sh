db1809453863e9aa6a76f92ba416e57446c9d1e8
Matias Muhonen <mmu@iki.fi>
2014-03-14 17:19:52 +0200

    Add a script to generate the API docs.

---
+++ makedoc.sh
@@ -0,0 +1,26 @@
+#! /bin/sh
+
+##
+## A helper script to generate the API documentation.
+##
+
+appledoc=$(which appledoc)
+helpdir="Help"
+
+if [ ! -x "$appledoc" ] ; then
+    echo "Need appledoc for running. Download and install https://github.com/tomaz/appledoc"
+    exit 1
+fi
+
+if [ -d "$helpdir" ]; then
+    rm -rf "$helpdir"
+fi
+
+$appledoc --project-name "FreeStreamer" \
+--project-company "Matias Muhonen" \
+--company-id "net.muhonen" \
+--ignore "*.m" \
+--ignore "*.mm" \
+--create-html \
+--no-create-docset \
+--output "$helpdir" "Common" "FreeStreamerMobile" "FreeStreamerDesktop"
