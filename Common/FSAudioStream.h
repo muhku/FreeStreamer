45934a75c920b4d05f921814dd271f6d29c8e4e5
Matias Muhonen <mmu@iki.fi>
2014-08-20 10:03:38 +0300

    Bump version to 2.4.0 (due to the API change)

--- Common/FSAudioStream.h
+++ Common/FSAudioStream.h
@@ -17,12 +17,12 @@
 /**
  * The minor version of the current release.
  */
-#define FREESTREAMER_VERSION_MINOR          3
+#define FREESTREAMER_VERSION_MINOR          4
 
 /**
  * The reversion of the current release
  */
-#define FREESTREAMER_VERSION_REVISION       2
+#define FREESTREAMER_VERSION_REVISION       0
 
 /**
  * Follow this notification for the audio stream state changes.
