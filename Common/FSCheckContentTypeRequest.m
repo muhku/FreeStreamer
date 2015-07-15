f31d3429775bdd6d43119882410f35734e7149a9
Pawel Szot <pszot@concisesoftware.com>
2015-07-15 11:09:13 +0200

    ignore content type if request code was not successful
    Cleanup a bit
    
    Also use the URL if the content type cannot be resolved.

--- Common/FSCheckContentTypeRequest.m
+++ Common/FSCheckContentTypeRequest.m
@@ -104,36 +104,50 @@
     _format = kFSFileFormatUnknown;
     _playlist = NO;
     
-    if ([_contentType isEqualToString:@"audio/mpeg"]) {
-        _format = kFSFileFormatMP3;
-    } else if ([_contentType isEqualToString:@"audio/x-wav"]) {
-        _format = kFSFileFormatWAVE;
-    } else if ([_contentType isEqualToString:@"audio/x-aifc"]) {
-        _format = kFSFileFormatAIFC;
-    } else if ([_contentType isEqualToString:@"audio/x-aiff"]) {
-        _format = kFSFileFormatAIFF;
-    } else if ([_contentType isEqualToString:@"audio/x-m4a"]) {
-        _format = kFSFileFormatM4A;
-    } else if ([_contentType isEqualToString:@"audio/mp4"]) {
-        _format = kFSFileFormatMPEG4;
-    } else if ([_contentType isEqualToString:@"audio/x-caf"]) {
-        _format = kFSFileFormatCAF;
-    } else if ([_contentType isEqualToString:@"audio/aac"] ||
-               [_contentType isEqualToString:@"audio/aacp"]) {
-        _format = kFSFileFormatAAC_ADTS;
-    } else if ([_contentType isEqualToString:@"audio/x-mpegurl"] ||
-               [_contentType isEqualToString:@"application/x-mpegurl"]) {
-        _format = kFSFileFormatM3UPlaylist;
-        _playlist = YES;
-    } else if ([_contentType isEqualToString:@"audio/x-scpls"] ||
-               [_contentType isEqualToString:@"application/pls+xml"]) {
-        _format = kFSFileFormatPLSPlaylist;
-        _playlist = YES;
-    } else if ([_contentType isEqualToString:@"text/xml"] ||
-               [_contentType isEqualToString:@"application/xml"]) {
-        _format = kFSFileFormatXML;
-        _xml = YES;
+    NSInteger statusCode = ((NSHTTPURLResponse *)response).statusCode;
+    
+    if (statusCode >= 200 && statusCode <= 299) {
+        // Only use the content type if the response indicated success (2xx)
+        
+        if ([_contentType isEqualToString:@"audio/mpeg"]) {
+            _format = kFSFileFormatMP3;
+        } else if ([_contentType isEqualToString:@"audio/x-wav"]) {
+            _format = kFSFileFormatWAVE;
+        } else if ([_contentType isEqualToString:@"audio/x-aifc"]) {
+            _format = kFSFileFormatAIFC;
+        } else if ([_contentType isEqualToString:@"audio/x-aiff"]) {
+            _format = kFSFileFormatAIFF;
+        } else if ([_contentType isEqualToString:@"audio/x-m4a"]) {
+            _format = kFSFileFormatM4A;
+        } else if ([_contentType isEqualToString:@"audio/mp4"]) {
+            _format = kFSFileFormatMPEG4;
+        } else if ([_contentType isEqualToString:@"audio/x-caf"]) {
+            _format = kFSFileFormatCAF;
+        } else if ([_contentType isEqualToString:@"audio/aac"] ||
+                   [_contentType isEqualToString:@"audio/aacp"]) {
+            _format = kFSFileFormatAAC_ADTS;
+        } else if ([_contentType isEqualToString:@"audio/x-mpegurl"] ||
+                   [_contentType isEqualToString:@"application/x-mpegurl"]) {
+            _format = kFSFileFormatM3UPlaylist;
+            _playlist = YES;
+        } else if ([_contentType isEqualToString:@"audio/x-scpls"] ||
+                   [_contentType isEqualToString:@"application/pls+xml"]) {
+            _format = kFSFileFormatPLSPlaylist;
+            _playlist = YES;
+        } else if ([_contentType isEqualToString:@"text/xml"] ||
+                   [_contentType isEqualToString:@"application/xml"]) {
+            _format = kFSFileFormatXML;
+            _xml = YES;
+        } else {
+#if defined(DEBUG) || (TARGET_IPHONE_SIMULATOR)
+            NSLog(@"FSCheckContentTypeRequest: Cannot resolve %@, guessing the content type by URL: %@", _contentType, _url);
+#endif
+            [self guessContentTypeByUrl:response];
+        }
     } else {
+#if defined(DEBUG) || (TARGET_IPHONE_SIMULATOR)
+        NSLog(@"FSCheckContentTypeRequest: Invalid HTTP status code received %li, guessing the content type by URL: %@", (long)statusCode, _url);
+#endif
         [self guessContentTypeByUrl:response];
     }
     
