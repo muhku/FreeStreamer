f2203d3decadd0ff6e4359c44bcdf1e2de03d1da
Cory Smith <cory.m.smith@gmail.com>
2016-01-01 23:59:51 -0700

    Removing unavailable simulator version from build command

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
 set -e
-xctool project FreeStreamerMobile.xcodeproj -scheme "FreeStreamerMobile" build -sdk iphonesimulator8.1 -arch i386 ONLY_ACTIVE_ARCH=NO
+xctool project FreeStreamerMobile.xcodeproj -scheme "FreeStreamerMobile" build -sdk iphonesimulator -arch i386 ONLY_ACTIVE_ARCH=NO
 xctool project FreeStreamerDesktop.xcodeproj -scheme "FreeStreamerDesktop" build -arch x86_64 ONLY_ACTIVE_ARCH=NO
