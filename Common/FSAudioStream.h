59af22749aa5522007b25a5e2b193f09e6901652
Matias Muhonen <mmu@iki.fi>
2016-02-09 09:19:59 +0200

    Bump to 3.4.5

--- Common/FSAudioStream.h
+++ Common/FSAudioStream.h
@@ -22,7 +22,7 @@
 /**
  * The reversion of the current release
  */
-#define FREESTREAMER_VERSION_REVISION       4
+#define FREESTREAMER_VERSION_REVISION       5
 
 /**
  * Follow this notification for the audio stream state changes.
