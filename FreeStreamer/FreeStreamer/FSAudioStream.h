b19945deb46f0dac3ea84b86f86c9ab90c1517da
Matias Muhonen <mmu@iki.fi>
2016-06-26 17:28:24 +0300

    Bump to 3.5.8

--- FreeStreamer/FreeStreamer/FSAudioStream.h
+++ FreeStreamer/FreeStreamer/FSAudioStream.h
@@ -22,7 +22,7 @@
 /**
  * The reversion of the current release
  */
-#define FREESTREAMER_VERSION_REVISION       7
+#define FREESTREAMER_VERSION_REVISION       8
 
 /**
  * Follow this notification for the audio stream state changes.
