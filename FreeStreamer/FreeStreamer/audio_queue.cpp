muhku/FreeStreamer

commit f03be2e4450b3a9e8fa4bbbcefa2ec43b929bb93

fix: After finish buffer, paused state will become playing.

FreeStreamer/FreeStreamer/audio_queue.cpp
@@ -127,6 +127,11 @@ void Audio_Queue::stop()
     stop(true);
 }
     
+Audio_Queue::State Audio_Queue::state()
+{
+    return m_state;
+}
+    
 float Audio_Queue::volume()
 {
     if (!m_outAQ) {

FreeStreamer/FreeStreamer/audio_queue.h
@@ -41,6 +41,8 @@ public:
     void stop(bool stopImmediately);
     void stop();
     
+    State state();
+    
     float volume();
     
     void setVolume(float volume);

FreeStreamer/FreeStreamer/audio_stream.cpp
@@ -1517,7 +1517,14 @@ bool Audio_Stream::decoderShouldRun()
 {
     const Audio_Stream::State state = this->state();
     
+    /* if audio queue paused, decoder should not run */
+    bool isAudioQueuePaused = false;
+    if (m_audioQueue) {
+        isAudioQueuePaused = m_audioQueue->state() == m_audioQueue->PAUSED;
+    }
+    
     pthread_mutex_lock(&m_streamStateMutex);
+    
     
     if (m_preloading ||
         !m_decoderShouldRun ||
@@ -1528,7 +1535,8 @@ bool Audio_Stream::decoderShouldRun()
         state == SEEKING ||
         state == FAILED ||
         state == PLAYBACK_COMPLETED ||
-        m_dstFormat.mBytesPerPacket == 0) {
+        m_dstFormat.mBytesPerPacket == 0 ||
+        isAudioQueuePaused ) {
         pthread_mutex_unlock(&m_streamStateMutex);
         return false;
     } else {

commit 99e2d44634b9a4e32d1c784da6018eed762769d4

Issue #320 - fix audio queue state checking
Commit f03be2e4450b3a9e8fa4bbbcefa2ec43b929bb93 introduced crashing due to lacking mutex
in the state get/set

FreeStreamer/FreeStreamer/audio_queue.cpp
@@ -111,12 +111,14 @@ void Audio_Queue::start()
     
 void Audio_Queue::pause()
 {
-    if (m_state == RUNNING) {
+    const State state = currentState();
+    
+    if (state == RUNNING) {
         if (AudioQueuePause(m_outAQ) != 0) {
             AQ_TRACE("%s: AudioQueuePause failed!\n", __PRETTY_FUNCTION__);
         }
         setState(PAUSED);
-    } else if (m_state == PAUSED) {
+    } else if (state == PAUSED) {
         AudioQueueStart(m_outAQ, NULL);
         setState(RUNNING);
     }
@@ -314,6 +316,17 @@ void Audio_Queue::init()
         setVolume(m_initialOutputVolume);
     }
 }
+    
+Audio_Queue::State Audio_Queue::currentState()
+{
+    State s = UNKNOWN;
+    
+    pthread_mutex_lock(&m_mutex);
+    s = m_state;
+    pthread_mutex_unlock(&m_mutex);
+    
+    return s;
+}
 
 void Audio_Queue::handleAudioPackets(UInt32 inNumberBytes, UInt32 inNumberPackets, const void *inInputData, AudioStreamPacketDescription *inPacketDescriptions)
 {
@@ -398,7 +411,9 @@ void Audio_Queue::cleanup()
     
     Stream_Configuration *config = Stream_Configuration::configuration();
     
-    if (m_state != IDLE) {
+    const State state = currentState();
+    
+    if (state != IDLE) {
         AQ_TRACE("%s: attemping to cleanup the audio queue when it is still playing, force stopping\n",
                  __PRETTY_FUNCTION__);
         
@@ -426,12 +441,18 @@ void Audio_Queue::cleanup()
     
 void Audio_Queue::setState(State state)
 {
+    pthread_mutex_lock(&m_mutex);
+    
     if (m_state == state) {
+        pthread_mutex_unlock(&m_mutex);
+        
         /* We are already in this state! */
         return;
     }
     
     m_state = state;
+    
+    pthread_mutex_unlock(&m_mutex);
     
     if (m_delegate) {
         m_delegate->audioQueueStateChanged(m_state);

FreeStreamer/FreeStreamer/audio_queue.h
@@ -23,7 +23,8 @@ public:
     enum State {
         IDLE,
         RUNNING,
-        PAUSED
+        PAUSED,
+        UNKNOWN
     };
     
     Audio_Queue();
@@ -32,6 +33,8 @@ public:
     bool initialized();
     
     void init();
+    
+    State currentState();
     
     // Notice: the queue blocks if it has no free buffers
     void handleAudioPackets(UInt32 inNumberBytes, UInt32 inNumberPackets, const void *inInputData, AudioStreamPacketDescription *inPacketDescriptions);

FreeStreamer/FreeStreamer/audio_stream.cpp
@@ -1523,6 +1523,7 @@ void Audio_Stream::stateSetTimerCallback(CFRunLoopTimerRef timer, void *info)
 bool Audio_Stream::decoderShouldRun()
 {
     const Audio_Stream::State state = this->state();
+    const Audio_Queue::State audioQueueState = (m_audioQueue ? this->audioQueue()->currentState() : Audio_Queue::UNKNOWN);
     
     pthread_mutex_lock(&m_streamStateMutex);
     
@@ -1535,7 +1536,8 @@ bool Audio_Stream::decoderShouldRun()
         state == SEEKING ||
         state == FAILED ||
         state == PLAYBACK_COMPLETED ||
-        m_dstFormat.mBytesPerPacket == 0) {
+        m_dstFormat.mBytesPerPacket == 0 ||
+        audioQueueState == Audio_Queue::PAUSED) {
         pthread_mutex_unlock(&m_streamStateMutex);
         return false;
     } else {