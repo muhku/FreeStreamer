ceb30f4a719ed4cc45fa0a338d8582a47dd64bf2
Matias Muhonen <mmu@iki.fi>
2018-02-19 23:49:53 +0200

    Use xcodebuild

--- build.sh
+++ build.sh
@@ -1,4 +1,8 @@
 #! /bin/sh
 set -e
-xctool project FreeStreamerMobile/FreeStreamerMobile.xcodeproj -scheme "FreeStreamerMobile" build -sdk iphonesimulator -arch i386 ONLY_ACTIVE_ARCH=NO
-xctool project FreeStreamerDesktop/FreeStreamerDesktop.xcodeproj -scheme "FreeStreamerDesktop" build -arch x86_64 ONLY_ACTIVE_ARCH=NO
+xcodebuild -project FreeStreamerMobile/FreeStreamerMobile.xcodeproj \
+	-scheme "FreeStreamerMobile" build \
+	-sdk iphonesimulator -arch i386 ONLY_ACTIVE_ARCH=NO
+xcodebuild -project FreeStreamerDesktop/FreeStreamerDesktop.xcodeproj \
+	-scheme "FreeStreamerDesktop" build \
+	-arch x86_64 ONLY_ACTIVE_ARCH=NO
